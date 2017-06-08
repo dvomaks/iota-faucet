59996a6451616d0025af7a53a8e4fe7898411568
ouya <alexander@andlabs.eu>
2017-06-09 01:40:13 +0200

    setting up a basic html layout and iota lib

---
+++ js/send.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    //
+    //  Instantiate IOTA
+    //
+    var iota = new IOTA({
+      'provider'  : 'http://sandbox.iotatoken.com/api/v1/',
+      'sandbox'   :  true,
+      'token'     : 'EXAMPLE-TOKEN-HERE'
+    });
+
+    var seed;
+    var balance = 0;
+
+    // we have a bank - so seed is static and secret!
+    // TODO one mainnet is back online - generate SEED
+    function setSeed(value) {
+        seed = "";
+    }
+
+    //
+    // Gets the addresses and transactions of an account
+    // As well as the current balance
+    //  Automatically updates the HTML on the site
+    //
+    function getAccountInfo() {
+        // Command to be sent to the IOTA API
+        // Gets the latest transfers for the specified seed
+        iota.api.getAccountData(seed, function(e, accountData) {
+
+            console.log("Account data", accountData);
+            balance = accountData.balance;
+
+            // Update total balance
+            $("#iota__balance").html(balance);
+        })
+    }
+
+
+    //
+    //  Makes a new transfer for the specified seed
+    //  Includes message and value
+    //
+    function sendTransfer(address, value, messageTrytes) {
+
+        var transfer = [{
+            'address': address,
+            'value': parseInt(value),
+            'message': "iota-faucet"
+        }]
+
+        console.log("Sending Transfer", transfer);
+
+        // We send the transfer from this seed, with depth 4 and minWeightMagnitude 18
+        iota.api.sendTransfer(seed, 4, 18, transfer, function(e) {
+
+            if (e){
+              console.log(e)
+            } else {
+                console.log("Successfully sent 1 IOTA to " + address)
+                balance = balance - value;
+                $("#iota__balance").html(balance);
+            }
+        })
+    }
+});
